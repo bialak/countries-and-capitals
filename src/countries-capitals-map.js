bbc01b46bdfa64db9535290cc2fafddee532cdeb
Klaudia Biala <klaudiabiaa@gmail.com>
2023-12-07 08:58:30 +0100

    init

---
+++ src/countries-capitals-map.js
@@ -0,0 +1,14 @@
+const countriesCapitalsMap = {
+	Germany: "Berlin",
+	Poland: "Warszawa",
+	Albania: "Tirana",
+	Austria: "Wiedeń",
+	Belgia: "Brukselia",
+	Chorwacja: "Zagrzeb",
+	Dania: "Kopenhaga",
+	Czechy: "Praga",
+	Francja: "Paryż",
+	Estonia: "Talin",
+};
+
+export default countriesCapitalsMap;
